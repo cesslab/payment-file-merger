39903152eb5dc410edf36316f4f575b9c5e5110b
aaruff <anwarruff@gmail.com>
2019-12-16 15:54:00 -0500

    fixed merge issue

--- merge.py
+++ merge.py
@@ -1,18 +1,20 @@
 import pandas as pd
 import os
-import datetime
+import time
 
 num_subjects = 14
 
 path = os.path.dirname(os.path.abspath(__file__))
-print(path)
-df_ztree = pd.read_csv(os.path.join(path, 'files', 'ztree.pay'), sep='\t', skiprows=[num_subjects + 1])
 
-df_otree = pd.read_excel(os.path.join(path,'files', 'otree.xlsx'), usecols=['Label', 'Total'])
-df_otree['Label'] = df_otree['Label'].str.lower()
+df_ztree = pd.read_csv(os.path.join(path, 'in', 'ztree.pay'), sep='\t', skiprows=[num_subjects + 1])
+df_otree = pd.read_excel(os.path.join(path, 'in', 'otree.xlsx'), usecols=['Label', 'Total'])
 
-df_ztree['Profit'] = df_otree['Total'] + df_ztree['Profit']
-df_ztree['Profit'] = df_ztree['Profit'].round(decimals=2)
+payoffs = df_otree.merge(df_ztree, left_on='Label', right_on='Computer')
+payoffs['Final_Profit'] = payoffs['Total'] + payoffs['Profit']
+print(payoffs)
 
-file_name = 'payment_{}.pay'.format(datetime.datetime.now().isoformat())
+df_ztree['Profit'] = payoffs['Final_Profit'].round(decimals=2)
+df_ztree.sort_values(by=['Computer'], inplace=True)
+
+file_name = 'payment_{}.pay'.format(time.strftime('%Y%m%d_%H%M%S'))
 df_ztree.to_csv(os.path.join(path, 'out', file_name), index=False, sep='\t')
